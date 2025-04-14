52d3dab4decf046816a3a17ce9a3b3392f3881cf
Pankaj Patidar <pankaj.patidar@ideavate.com>
2025-04-14 17:46:19 +0530

    Test data

--- src/main/java/base/PredefinedActions.java
+++ src/main/java/base/PredefinedActions.java
@@ -6,7 +6,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.UUID;
 
 import io.cucumber.java.en_old.Ac;
 import org.apache.maven.model.Model;
@@ -77,13 +76,6 @@ public class PredefinedActions {
             chromeOptions.addArguments("--headless");
             edgeOptions.addArguments("--headless");
             firefoxOptions.addArguments("--headless");
-        }
-        // Avoid profile conflict for Edge
-        if (browser.equalsIgnoreCase("edge")) {
-            String uniqueUserDataDir = "/tmp/edge-profile-" + UUID.randomUUID();
-            edgeOptions.addArguments("--user-data-dir=" + uniqueUserDataDir);
-            edgeOptions.addArguments("--no-sandbox");
-            edgeOptions.addArguments("--disable-dev-shm-usage");
         }
 
 
